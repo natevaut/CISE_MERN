370522cd8c0e0f0b5564cf6fa89591b942a4f976
nate <jmy3092@autuni.ac.nz>
2023-08-11 16:32:32 +1200

    CORS throughput

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
 
 const app = express();
 
 // Connect Database
 connectDB();
 
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+// allows Express to read data sent using a POST or PUT request. It is used for recognizing incoming objects as JSON objects.
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
